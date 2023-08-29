78a6be4237458449c81b0475caaf6a051f9b0d9c
Nicolas Vargas <nicolasvargas846@gmail.com>
2023-08-29 10:39:40 -0500

    Adding header slider and nav menu

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <home-header/>
+</template>
+
+<script>
+import HomeHeader from '@/components/HomeHeader.vue'
+export default {
+  components: {  HomeHeader },
+
+}
+</script>
+
+<style>
+
+</style>
